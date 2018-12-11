1b7b9b5a1e1e4f38dac77d061e163d3dfd7caddd
watercolorhorse <29869241+llcook@users.noreply.github.com>
2018-12-10 23:12:52 -0700

    Cleaned code. Added some functionality to doWhatItSays, but the code is not reusable for all LIRI functions.

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
-//At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
+// read and set any environment variables with the dotenv package
 require("dotenv").config();
 
-// Add the code required to import the keys.js file and store it in a variable.
+// add code required to import keys.js file store in variable
 var keys = require("./keys");
 
 var fs = require("fs");
@@ -16,35 +16,36 @@ var userInput = process.argv.slice(3).join("+");
 
 var info;
 
-// Make it so liri.js can take in one of the following commands:
+// LIRI PROCESS
 
-switch (action) {
+function runLiri(action) {
 
-    // concert-this
-    case "concert-this":
-    concertThis(userInput);
-    break;
+    switch (action) {
 
-    // spotify-this-song
-    case "spotify-this-song":
-    spotifyThisSong(userInput);
-    break;
+        // concert-this
+        case "concert-this":
+            concertThis(userInput);
+            break;
 
-    // movie-this
-    case "movie-this":
-    movieThis(userInput);
-    break;
+        // spotify-this-song
+        case "spotify-this-song":
+            spotifyThisSong(userInput);
+            break;
 
-    // do-what-it-says
-    case "do-what-it-says":
-    doWhatItSays(userInput);
-    break;
+        // movie-this
+        case "movie-this":
+            movieThis(userInput);
+            break;
 
+        // do-what-it-says
+        case "do-what-it-says":
+            doWhatItSays(userInput);
+            break;
+
+    };
 };
 
-// `node liri.js concert-this <artist/band name here>`
-
-// * Date of the Event (use moment to format this as "MM/DD/YYYY")
+// LIRI FUNCTIONS
 
 function concertThis() {
 
@@ -52,81 +53,103 @@ function concertThis() {
 
     var queryUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=" + bandsKey;
 
+    // query Bandsintown via user request
     axios.get(queryUrl)
-    .then(function(response) {
+        .then(function (response) {
 
-        info = response.data;
+            info = response.data;
 
-        for (var i = 0; i < response.data.length; i++) {
-            var artist = info[i].lineup;
-            var venue = info[i].venue.name;
-            var location = info[i].venue.city;
-            var date = info[i].datetime;
-            
-            console.log(`::::::::::::::::::\n\nArtist: ${artist}\nVenue: ${venue}\nLocation: ${location}\nDate: ${date}\n`);
-        }
-    })
+            for (var i = 0; i < response.data.length; i++) {
+                var artist = info[i].lineup;
+                var venue = info[i].venue.name;
+                var location = info[i].venue.city;
+                var date = info[i].datetime;
+                    // TO-DO: use moment to format date as "MM/DD/YYYY")
+
+                console.log(`::::::::::::::::::\n\nArtist: ${artist}\nVenue: ${venue}\nLocation: ${location}\nDate: ${date}\n`);
+            }
+        })
 
 }
-
-// `node liri.js spotify-this-song '<song name here>'`
-
-// * This will show the following information about the song in your terminal/bash window * Artist(s) * The song's name * A preview link of the song from Spotify * The album that the song is from * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
 function spotifyThisSong() {
 
-    spotify.search({ type: "track", query: userInput, limit: 10}, function(err, data) {
+    // if user doesn't input a request, it returns this as default
+    if (!userInput) {
+        userInput = "careful with that axe eugene"
+        console.log(`\n\nYou didn't give me anything to search. Here's some psychedelic rock.\n\n`)
+    }
+
+    // query Spotify via user request
+    spotify.search({ type: "track", query: userInput, limit: 10 }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
-        }
+            return console.log('Error occurred: ' + err);
+        } else {
 
-        var info = data.tracks.items;
-        var artist = info[0].artists[0].name;
-        var song = info[0].name
-        var preview = info[0].preview_url;
-        var album = info[0].album.name;
-       
-      console.log(`::::::::::::::::::\n\nArtist: ${artist}\nSong: ${song}\nSpotify preview: ${preview}\nAlbum: ${album}\n`);
+            for (var i = 0; i < 10; i++) {
+                var info = data.tracks.items[i];
+                var artist = info.artists[0].name;
+                var song = info.name
+                var preview = info.preview_url;
+                var album = info.album.name;
 
-      });
+                console.log(`::::::::::::::::::\n\nArtist: ${artist}\nSong: ${song}\nAlbum: ${album}\nSpotify preview: ${preview}\n`);
+            }
+        };
+    });
 
 }
 
-// `node liri.js movie-this '<movie name here>'`
-
 function movieThis() {
 
+    // if user doesn't input a request, it returns this as default
+    if (!userInput) {
+        userInput = "the color of pomegranates"
+        console.log(`\n\nYou didn't give me anything to search. Have you seen this?.\n\n`)
+    }
+
+    // query OMDB via user request
     var movieKey = "trilogy";
 
     var queryUrl = "http://www.omdbapi.com/?apikey=" + movieKey + "&t=" + userInput;
 
     axios.get(queryUrl)
-    .then(function(response) {
-        
-        info = response.data;
-        var title = info.Title;
-        var year = info.Year;
-        var imdbRate = info.imdbRating;
-        var tomatoRate = info.Ratings[1].Value;
-        var country = info.Country;
-        var lang = info.Language;
-        var plot = info.Plot;
-        var actors = info.Actors;
+        .then(function (response) {
 
-        console.log(`::::::::::::::::::\n\nFilm: ${title}\nYear: ${year}\nIMDB rating: ${imdbRate}\nRotten Tomatoes rating: ${tomatoRate}\nCountry: ${country}\nLanguage: ${lang}\nPlot: ${plot}\nActors: ${actors}\n\n::::::::::::::::::`);
+            info = response.data;
+            var title = info.Title;
+            var year = info.Year;
+            var imdbRate = info.imdbRating;
+            var tomatoRate = info.Ratings[1].Value;
+            var country = info.Country;
+            var lang = info.Language;
+            var plot = info.Plot;
+            var actors = info.Actors;
 
-    })
+            console.log(`::::::::::::::::::\n\nFilm: ${title}\nYear: ${year}\nCountry: ${country}\nPlot: ${plot}\nActors: ${actors}\nLanguage: ${lang}\nIMDB rating: ${imdbRate}\nRotten Tomatoes rating: ${tomatoRate}\n\n::::::::::::::::::`);
+
+        })
 }
-
-//`node liri.js do-what-it-says`
-
-// * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-// * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-// * Edit the text in random.txt to test out the feature for movie-this and concert-this.
 
 function doWhatItSays() {
 
+    // read random.txt
+    fs.readFile("random.txt", "utf8", function (error, data) {
+    
+        // turn the data into an array, divided by comma, with index 0 and 1
+        var output = data.split(",");
+        action = output[0];
+        userInput = output[1];
+
+        // plug the random.txt request into a function
+        if (action === "spotify-this-song") {
+            spotifyThisSong(action, userInput);
+        }
+
+        // TO DO: It should read the text in `random.txt` to call any of LIRI's commands
+
+    });
+
 }
 
+runLiri(action);
